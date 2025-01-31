919be170a762e6e2d81ace157123924dc44bb002
obaidallah <obaidalqershi1@gmail.com>
2025-01-31 17:08:27 +0300

    integrate authentication

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import * as SplashScreen from "expo-splash-screen";
 import './global.css'
 import {useFonts} from 'expo-font';
 import { useEffect } from "react";
+import GlobalProvider from "@/lib/global-provider";
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
     "Rubik-Bold": require('../assets/fonts/Rubik-Bold.ttf'),
@@ -20,5 +21,6 @@ export default function RootLayout() {
   }, [fontsLoaded])
   if(!fontsLoaded) return null;
   
-  return <Stack screenOptions={{headerShown: false}} />;
+  return (<GlobalProvider> <Stack screenOptions={{headerShown: false}} />
+  </GlobalProvider>)
 }
