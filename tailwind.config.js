18c09409f1e8af1773a5d2e18c4a3c90534bd8be
obaidallah <obaidalqershi1@gmail.com>
2025-01-31 15:29:09 +0300

    login screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,23 @@ module.exports = {
         "rubik-semibold": ["Rubik-SemiBold", "sans-serif"],
         "rubik-light": ["Rubik-Light", "sans-serif"],
       },
+      colors: {
+        primary: {
+          100: "#0061FF0A",
+          200: "#0061FF1A",
+          300: "#0061FF",
+        },
+        accent: {
+          100: "#FBFBFD",
+        },
+        black: {
+          DEFAULT: "#000000",
+          100: "#8C8E98",
+          200: "#666876",
+          300: "#191D31",
+        },
+        danger: "#F75555",
+      },
     },
   },
   plugins: [],
