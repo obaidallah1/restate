360092c2fd84a1e312252638668d362831ac9beb
obaidallah <obaidalqershi1@gmail.com>
2025-01-31 15:10:35 +0300

    load

---
+++ app/sign-in.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <View>
+      <Text>sign-in</Text>
+      <Text className="font-bold text-lg my-10 text-red-700">sign-in</Text>
+    </View>
+  )
+}
+
+export default SignIn
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik-Regular", "sans-serif"],
+        "rubik-bold": ["Rubik-Bold", "sans-serif"],
+        "rubik-extrabold": ["Rubik-ExtraBold", "sans-serif"],
+        "rubik-medium": ["Rubik-Medium", "sans-serif"],
+        "rubik-semibold": ["Rubik-SemiBold", "sans-serif"],
+        "rubik-light": ["Rubik-Light", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
