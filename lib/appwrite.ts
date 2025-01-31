159ca60482ee16f08f8abe6e5328ddb314f67632
obaidallah <obaidalqershi1@gmail.com>
2025-01-31 21:22:38 +0300

    still trying to fix the auth

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -9,50 +9,56 @@ export const config = {
 }
 
 export const client = new Client();
-client.setEndpoint(config.endpoint!)
-.setProject(config.projectId!)
-.setPlatform(config.platform!)
+client
+  .setEndpoint(config.endpoint!)
+  .setProject(config.projectId!)
+  .setPlatform(config.platform!);
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
 
-
-export async function login(){
+export async function login() {
     try {
-        const redirectUri = Linking.createURL('/');
-        const response = await account.createOAuth2Token(OAuthProvider.Google, redirectUri);
-
-    if(!response) throw new Error('Failed to login');
-    const browserResult = await openAuthSessionAsync(
+      const redirectUri = Linking.createURL("/");
+  
+      const response = await account.createOAuth2Token(
+        OAuthProvider.Google,
+        redirectUri
+      );
+      if (!response) throw new Error("Create OAuth2 token failed");
+  
+      const browserResult = await openAuthSessionAsync(
         response.toString(),
         redirectUri
-    )
-    if(browserResult.type != 'success') throw new Error('Failed to login!');
-    const url = new URL(browserResult.url);
-    const secret = url.searchParams.get('secret')?.toString()
-    const userId = url.searchParams.get('userId')?.toString()
-
-    if(!secret || !userId) throw new Error('Failed to login!');
-
-    const session = await account.createSession(userId, secret);
-    if(!session) throw new Error('Failed to create session!');
-    return true;
-    
+      );
+      if (browserResult.type !== "success")
+        throw new Error("Create OAuth2 token failed");
+  
+      const url = new URL(browserResult.url);
+      const secret = url.searchParams.get("secret")?.toString();
+      const userId = url.searchParams.get("userId")?.toString();
+      if (!secret || !userId) throw new Error("Create OAuth2 token failed");
+  
+      const session = await account.createSession(userId, secret);
+      console.log("Session created:", session);
+      if (!session) throw new Error("Failed to create session");
+  
+      return true;
     } catch (error) {
-        console.error(error);
-        return false
+      console.error(error);
+      return false;
     }
-}
-export async function logout(){
+  }
+  
+  export async function logout() {
     try {
-        await account.deleteSession('current');
-        return true;
-        
+      const result = await account.deleteSession("current");
+      return result;
     } catch (error) {
-        console.error(error);
-        return false       
+      console.error(error);
+      return false;
     }
-}
+  }
 
 export async function getCurrentUser(){
     try {
